e2ce742ae1d56b4b4f895dbacfcb671a9800ed52
Aimeur Amin <mohammedamin.aimeur@gmail.com>
2022-11-02 17:56:37 +0000

    fixes styles

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,11 +3,13 @@ import Sidebar from "@components/Sidebar";
 
 export default function MainLayout(Content:any) {
   return () => (
-    <div className="flex bg-dark-70 text-white-90 h-screen">
+    <div className="flex h-screen bg-dark-70 text-white-90">
       <Sidebar />
-      <div className="flex-1">
+      <div className="flex flex-col flex-1">
         <Navbar />
-        <Content />
+        <div className="flex-1">
+          <Content />
+        </div>
       </div>
     </div>
   );
